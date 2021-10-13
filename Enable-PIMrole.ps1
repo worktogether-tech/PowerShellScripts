f28fd089d00d6023230a593e4551ccce469bf819
Arjan Cornelissen <arjancornelissen@users.noreply.github.com>
2021-10-13 22:22:18 +0200

    Update Enable-PIMrole.ps1
    Upgrade code to use the AzureADPreview module

--- Enable-PIMrole.ps1
+++ Enable-PIMrole.ps1
@@ -1,20 +1,27 @@
 <#
 .Synopsis
-   Enable or disable Priviledged role in Azure PIM
+    Enable or disable Priviledged role in Azure PIM
 .DESCRIPTION
-   With this script you can enable or disable privileged role assignments
-   You need to have the module Microsoft.Azure.ActiveDirectory.PIM.PSModule
+    With this script you can enable or disable privileged role assignments
+    You need to have the module AzureADPreview
    
-   To get all active roles use this command Get-PrivilegedRoleAssignment |? {$_.IsElevated -eq $true -and $_.IsPermanent -eq $false}
+    To get all active roles use this command
+    .\Enable-PIM.ps1 -ShowActive
 .EXAMPLE
-   .\Enable-PIMrole.ps1 -pimroles 'Exchange Administrator','SharePoint Service Administrator', 'User Administrator' -ticketnumber "nvt" -reason "changes" -duration 8
-   Enables roles Exchange Administrator, SharePoint Service Administrator and User Administrator
+    .\Enable-PIM.ps1 -pimroles 'Exchange Administrator','SharePoint Service Administrator', 'User Administrator' -ticketnumber "nvt" -reason "changes" -duration 8
+    Enables roles Exchange Administrator, SharePoint Service Administrator and User Administrator
 .EXAMPLE
-   .\Enable-PIMrole.ps1 -pimroles 'Exchange Administrator','SharePoint Service Administrator' -Disable
-   Disable roles Exchange Administrator and SharePoint Service Administrator
+    .\Enable-PIM.ps1 -pimroles 'Exchange Administrator','SharePoint Service Administrator', 'User Administrator' -ticketnumber "nvt" -reason "changes" -duration 8 -RenewRole
+    Renews the roles Exchange Administrator, SharePoint Service Administrator and User Administrator
 .EXAMPLE
-   .\Enable-PIMrole.ps1 -DisableAll
-   Disable all active roles
+    .\Enable-PIM.ps1 -pimroles 'Exchange Administrator','SharePoint Service Administrator' -Disable
+    Disable roles Exchange Administrator and SharePoint Service Administrator
+.EXAMPLE
+    .\Enable-PIM.ps1 -DisableAll
+    Disable all active roles
+.EXAMPLE
+    .\Enable-PIM.ps1 -ShowActive
+    Shows all active roles
 #>
 
 Param
@@ -22,7 +29,7 @@ Param
     # What PIM role should be activated
     [Parameter(Mandatory=$true, Position=1, ParameterSetName='Addrole')]
     [Parameter(ParameterSetName='Removerole')]
-    [ValidateSet("Application Administrator","Application Developer","Billing Administrator","Cloud Application Administrator","Cloud Device Administrator","Compliance Administrator","Conditional Access Administrator","CRM Service Administrator","Customer LockBox Access Approver","Desktop Analytics Administrator","Device Administrators","Directory Readers","Directory Writers","Exchange Administrator","Global Administrator","Guest Inviter","Information Protection Administrator","Intune Service Administrator","License Administrator","Message Center Reader","Password Administrator","Power BI Service Administrator","Privileged Role Administrator","Reports Reader","Security Administrator","Security Reader","Service Administrator","SharePoint Service Administrator","Skype for Business Administrator","Teams Communications Administrator","Teams Communications Support Engineer","Teams Communications Support Specialist","Teams Service Administrator","User Administrator")]
+    [ValidateSet("Search Administrator","External ID User Flow Attribute Administrator","Guest User","Power Platform Administrator","Cloud Application Administrator","Compliance Administrator","Security Administrator","Exchange Administrator","Restricted Guest User","Device Managers","Office Apps Administrator","Insights Business Leader","Desktop Analytics Administrator","Intune Administrator","Teams Devices Administrator","B2C IEF Policy Administrator","Dynamics 365 Administrator","Reports Reader","Partner Tier1 Support","License Administrator","Customer LockBox Access Approver","Security Reader","Security Operator","Global Administrator","Printer Administrator","Teams Administrator","External ID User Flow Administrator","Helpdesk Administrator","Azure Information Protection Administrator","Kaizala Administrator","Usage Summary Reports Reader","Skype for Business Administrator","Cloud Device Administrator","Message Center Reader","Privileged Authentication Administrator","Search Editor","Directory Readers","Hybrid Identity Administrator","Directory Writers","Guest Inviter","Password Administrator","Application Administrator","Device Join","Attack Payload Author","Azure AD Joined Device Local Administrator","Power BI Administrator","B2C IEF Keyset Administrator","Message Center Privacy Reader","Billing Administrator","Conditional Access Administrator","Teams Communications Administrator","External Identity Provider Administrator","Workplace Device Join","Attack Simulation Administrator","Authentication Administrator","Application Developer","Directory Synchronization Accounts","Network Administrator","Device Users","Partner Tier2 Support","Azure DevOps Administrator","Compliance Data Administrator","Privileged Role Administrator","Printer Technician","Insights Administrator","Service Support Administrator","SharePoint Administrator","Global Reader","Teams Communications Support Engineer","Teams Communications Support Specialist","Groups Administrator","User Administrator")]
     [string[]]$pimroles,
 
     # What is the ticketnumber
@@ -43,44 +50,101 @@ Param
 
     # should we remove all roles
     [Parameter(Mandatory=$true, ParameterSetName='RemoveAllroles')]
-    [switch]$DisableAll
+    [switch]$DisableAll,
+
+    # should we renew the roles
+    [Parameter(Mandatory=$false, ParameterSetName='Addrole')]
+    [switch]$RenewRole,
+
+    # Show Active Roles
+    [Parameter(Mandatory=$false, ParameterSetName='ShowActive')]
+    [switch]$ShowActive
 )
 
-#check if we are connected to PIM
-if([string]::IsNullOrEmpty((Show-PimServiceConnection).UserName))
+# Check if we are connected to AzureAD
+try 
 {
-    Connect-PimService
+    $tenantInfo = Get-AzureADCurrentSessionInfo -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    $tenantId = $tenantInfo.TenantId
+}
+catch
+{
+    Connect-AzureAD
+    $tenantId =(Get-AzureADCurrentSessionInfo).TenantId
 }
 
-if($DisableAll)
+$providerId = "aadRoles"
+$currentUser = (Get-AzureADUser -SearchString (Get-AzureADCurrentSessionInfo).Account).ObjectId
+
+$Allroledefs = Get-AzureADMSPrivilegedRoleDefinition -ProviderId $providerId -ResourceId $tenantId
+$indexedRolesId = @{}
+$i=0
+$Allroledefs.foreach({
+    $indexedRolesId["$($psitem.ExternalId)"] = $i
+    $i++
+})
+$indexedRolesName = @{}
+$i=0
+$Allroledefs.foreach({
+    $indexedRolesName["$($psitem.DisplayName)"] = $i
+    $i++
+})
+$myRoles = Get-AzureADMSPrivilegedRoleAssignment -ProviderId $providerId -ResourceId $tenantId -Filter "subjectId eq '$currentUser'"
+
+if($DisableAll.IsPresent)
 {
     # Disable all roles
     Write-Host "Disabling all active roles"
-    Get-PrivilegedRoleAssignment | Where-Object {$_.IsElevated -eq $true -and $_.IsPermanent -eq $false} | Disable-PrivilegedRoleAssignment
+    $myRoles | Where-Object {$_.AssignmentState -eq "Active" -and $_.null -ne $_.EndDateTime} | ForEach-Object {
+        Write-Host "Disabling $(($Allroledefs[($indexedRolesId[$_.RoleDefinitionId])]).DisplayName)" -ForegroundColor Green
+        $roleId = ($Allroledefs[($indexedRolesId[$_.RoleDefinitionId])]).ExternalId
+        $schedule = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedSchedule
+        Open-AzureADMSPrivilegedRoleAssignmentRequest -ProviderId $providerId -ResourceId $tenantId -RoleDefinitionId $roleId -SubjectId $currentUser -Type 'UserRemove' -AssignmentState 'Active' -Schedule $schedule | Out-Null
+    }    
+}
+elseif ($ShowActive.IsPresent) 
+{
+    Get-AzureADMSPrivilegedRoleAssignment -ProviderId $providerId -ResourceId $tenantId -Filter "subjectId eq '$currentUser' and assignmentState eq 'Active'" | ForEach-Object {
+        Write-Output "$(($Allroledefs[($indexedRolesId[$_.RoleDefinitionId])]).DisplayName) until $($_.EndDateTime.ToString('dd MMM yyyy HH:mm'))"
+    }
 }
 else
 {
+    $schedule = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedSchedule
+    $schedule.Type = "Once"
+    $schedule.StartDateTime = Get-Date
+    $schedule.endDateTime = $schedule.StartDateTime.AddHours($duration)
+
     foreach($pimrole in $pimroles)
     {
-        $roleAssignment = $null
-        $roleAssignment = Get-PrivilegedRoleAssignment | Where-Object {$_.RoleName -eq $pimrole}
-        if([string]::IsNullOrEmpty($roleAssignment))
+        $roleId = ($Allroledefs[($indexedRolesName[$pimrole])]).ExternalId
+        $myRole = $myRoles | Where-Object { $_.RoleDefinitionId -eq $roleId}
+        if($null -eq $myRole)
         {
-            Write-Error -Message "Cannot find $pimrole for your account"
+            Write-Error -Message "Cannot find '$pimrole' for your account"
         }
-        else
+        else 
         {
-            $roleid = $roleAssignment.RoleId
-            if($Disable)
+            $activeRole = $myRole |Where-Object{ $_.AssignmentState -eq "Active"}
+            if($Disable.IsPresent)
             {
-                Write-Host "Disabling $pimrole"
-                Disable-PrivilegedRoleAssignment -RoleId $roleid
+                Write-Host "Disabling $pimrole" -ForegroundColor Green
+                $schedule = New-Object Microsoft.Open.MSGraph.Model.AzureADMSPrivilegedSchedule
+                Open-AzureADMSPrivilegedRoleAssignmentRequest -ProviderId $providerId -ResourceId $tenantId -RoleDefinitionId $roleId -SubjectId $currentUser -Type 'UserRemove' -AssignmentState 'Active' -Schedule $schedule | Out-Null
+            }
+            elseif($null -ne $activeRole -and !$RenewRole.IsPresent)
+            {
+                Write-Host "$pimrole is already enabled, until $($activeRole.EndDateTime.ToString('dd MMM yyyy HH:mm'))" -ForegroundColor Yellow
             }
             else
             {
                 Write-Host "Enabling $pimrole for $duration hours"
-                Enable-PrivilegedRoleAssignment -TicketNumber $ticketnumber -Reason $reason -Duration $duration -RoleId $roleid
+                Open-AzureADMSPrivilegedRoleAssignmentRequest -ProviderId $providerId -ResourceId $tenantId -RoleDefinitionId $roleId -SubjectId $currentUser -Type 'UserAdd' -AssignmentState 'Active' -Schedule $schedule -reason "Ticketnumber: $ticketnumber; Reason: $reason" | Out-Null
+                $until = $schedule.endDateTime.ToString("dd MMM yyyy HH:mm")
+                Write-Host "Enabled until $until" -ForegroundColor Green
             }
         }
     }
+
+    Write-Host "Reconnect to Azure AD for the roles to become active" -ForegroundColor Yellow
 }
